1d6cdaec0c16220d7c703c49a9b8acd2628a016c
Rustam <locelot96@gmail.com>
2022-09-30 16:45:34 +0530

    first commit

---
+++ ads.py
@@ -0,0 +1,5 @@
+
+
+a = 1314141
+print(f"{a}\n\n"
+      f"huilo\n\n")
--- handlers/client.py
+++ handlers/client.py
@@ -3,6 +3,7 @@ from config import bot,dp
 from aiogram import types, Dispatcher
 from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 from database.db import sql_command_random
+from parseryr.Arkasha import parser
 
 async def meme(message: types.message):
     photes = ['media/meme1.jpg',
@@ -53,6 +54,18 @@ async def pin(message: types.Message):
 async def show_random_user(message: types.Message):
     await sql_command_random(message)
 
+async def films_parser(message: types.Message):
+    films = parser()
+    for fily in films:
+        await bot.send_message(
+            message.from_user.id,
+            # f"{fily['title']}\n\n"
+            f"{fily['year']}\n"
+            f"#{fily['gengre']}\n\n"
+            f"{fily['link']}"
+            # f"   {fily['desc']}\n"
+            # f"{fily['time']}"
+        )
 
 
 def register_handlers_client(dp: Dispatcher):
@@ -61,4 +74,6 @@ def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(dice_game, commands=['dice'])
     dp.register_message_handler(pin, commands=['pin'], commands_prefix='!/')
     dp.register_message_handler(show_random_user, commands=['get'])
+    dp.register_message_handler(films_parser, commands=["films"])
+
 
